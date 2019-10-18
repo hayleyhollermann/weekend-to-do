66dec0de4c08017b097040d79d1e7c8cd00f4efc
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-18 18:31:50 -0500

    get/put requests in client side js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,47 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('js and jq are ready');
+    $('#submitButton').on('click', addNewTask);
+    tasksToTable();
 }
+
+function loopTasks(array){
+    console.log('in loop');
+    $('#showTasks').empty();
+    for (taskToDo of array){
+        $('#showTasks').append(`
+          <tr data-id="${taskToDo.id}">
+          <td>${taskToDo.task}</td>
+          <td>${taskToDo.complete}</td>
+          <td><button class="delete">Delete</button></td>
+        </tr>`
+        )}
+}
+
+// GET
+function tasksToTable(){
+    console.log('getting tasks');
+    $.ajax({
+        type: 'GET',
+        url: '/tasks'
+      }).then(function(response){
+        loopTasks(response);
+      })
+}
+
+// POST
+function addNewTask(){
+    console.log('adding task')
+    let taskToSend = {
+        task: $('#taskInput').val(),
+      };
+      $.ajax({
+        type: 'POST',
+        url: '/tasks',
+        data: {
+          task: taskToSend.task
+        }
+      }).then(function(){
+        tasksToTable();
+      })
+};
