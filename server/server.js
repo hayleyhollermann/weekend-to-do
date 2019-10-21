c5cf24d8c7465d2f443f2925949c4a22941ebada
Hayley Hollermann <hayleyhollermann@gmail.com>
2019-10-21 11:48:08 -0500

    prep code for heroku

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
-const taskRouter = require('./routes/tasks.router')
+const taskRouter = require('./routes/tasks.router');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
